93e7a408f8da65c2de9f850e337a76a8db55adf5
Chris Biancone <chris.biancone@gmail.com>
2021-01-12 11:39:17 -0500

    i think i got nvcc to be recognized by the system

---
+++ CUDA-stuff/Gigabrot/kernel.cu
@@ -0,0 +1,28 @@
+﻿
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cstdio>
+#include <stdio.h>
+
+void CPUFunction()
+{
+	printf("hello from the Cpu.\n");
+}
+
+__global__
+void GPUFunction()
+{
+	printf("hello from the Gpu.\n");
+}
+
+int main()
+{
+	CPUFunction();
+
+	GPUFunction<<<1, 1 >>>();
+
+	cudaDeviceSynchronize();
+
+	return EXIT_SUCCESS;
+}
