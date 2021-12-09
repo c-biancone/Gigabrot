8ba5c8e45928685136e09ac1b4bca10f9bc46026
c-biancone <chris.biancone@gmail.com>
2021-12-08 22:55:06 -0500

    translate to c++

--- C++/Mandelbrot.h
+++ C++/Mandelbrot.h
@@ -1,9 +1,41 @@
-#ifndef GIGABROT_MANDELBROT_H
-#define GIGABROT_MANDELBROT_H
+#ifndef C___MANDELBROT_H
+#define C___MANDELBROT_H
 
-#include <complex.h>
-#include <math.h>
+#include <cmath>
+#include <complex>
+#include <algorithm>
+
+#define M_PI 3.14159265358979323846
+
+class Mandelbrot
+{
+public:
+
+    Mandelbrot(int iterMax, double xMin, double xMax, double yMin, double yMax);
+
+    int iterate(size_t iter);
+
+    void set_max_iter(size_t iterMax);
+
+    int iter;
+
+private:
+
+    const int iterMax;
+
+    std::complex<double> r;
+
+    std::complex<double> z;
+
+    std::complex<double> dC;
+
+    double q;
+
+    double cardioid;
+
+    const double bulb;
 
 
+};
 
-#endif //GIGABROT_MANDELBROT_H
+#endif //C___MANDELBROT_H
---
+++ C++/MandelbrotConfigAbstract.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chris on 12/7/2021.
+//
+
+#include "MandelbrotConfigAbstract.h"
